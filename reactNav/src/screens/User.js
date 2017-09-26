5486f1e8f8569ffae87f0240f644ea34ade1d003
sebastian gamboa <sebastiangamboa11@gmail.com>
2017-09-26 14:33:17 -0500

    react pruebas

--- reactNav/src/screens/User.js
+++ reactNav/src/screens/User.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text
+  Text,
+  TextInput,
+  StyleSheet,
 } from 'react-native';
 
 export default class User extends Component{
+	constructor(props) {
+    super(props);
+    this.state = { text: '' };
+	}
+	
 	render(){
 		return(
 			<View style={{ flex:1, justifyContent:'center', alignItems:'center'}}>
-				<Text>User</Text>
+				<View style={styles.form}>
+					<TextInput
+						style={styles.input1}
+						onChangeText={(text) => this.setState({text})}
+						value={this.state.text}
+						placeholder = 'nombre'
+					/>
+				</View>
 			</View>	
 			)
 	}
 }
+
+const styles = StyleSheet.create({
+  input1:{
+		width: 200,
+		height:30,
+		marginTop:15,
+		padding: 15,
+		
+
+	}
+});
