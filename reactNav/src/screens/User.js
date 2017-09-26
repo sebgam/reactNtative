232ac76c63b3e13f2c7638e906621a8b85bc24c6
sebastian gamboa <sebastiangamboa11@gmail.com>
2017-09-26 16:10:42 -0500

    functional user

--- reactNav/src/screens/User.js
+++ reactNav/src/screens/User.js
@@ -4,37 +4,81 @@ import {
   Text,
   TextInput,
   StyleSheet,
+  TouchableOpacity,
+  Alert,
+  Image
 } from 'react-native';
 
 export default class User extends Component{
-	constructor(props) {
-    super(props);
-    this.state = { text: '' };
+	constructor() {
+    super();
+    this.state = { 
+		nombre: '' ,
+		apellido: '' ,
+		edad: '' ,
+	};
 	}
 	
 	render(){
 		return(
 			<View style={{ flex:1, justifyContent:'center', alignItems:'center'}}>
 				<View style={styles.form}>
+					<Image source={require('../../public/images/user.png')}  style={{width: 100, height: 100}} />
 					<TextInput
 						style={styles.input1}
-						onChangeText={(text) => this.setState({text})}
-						value={this.state.text}
-						placeholder = 'nombre'
+						onChangeText={(nombre) => this.setState({nombre})}
+						value={this.state.nombre}
+						placeholder = 'Nombre'
 					/>
+					<TextInput
+						style={styles.input1}
+						onChangeText={(apellido) => this.setState({apellido})}
+						value={this.state.apellido}
+						placeholder = 'Apellido'
+					/>
+					<TextInput
+						style={styles.input1}
+						onChangeText={(edad) => this.setState({edad})}
+						value={this.state.edad}
+						placeholder = 'Edad'
+					/>
+					<TouchableOpacity 
+						onPress={this.enviar.bind(this)}
+						style={styles.enviar}
+					>
+						<Text style={{ color:'#fff', textAlign:'center'}}>Enviar</Text>
+					</TouchableOpacity>
 				</View>
 			</View>	
 			)
+	}
+
+	enviar(){
+		Alert.alert(
+			'Hola',
+			'Enviaste el formulario',
+			[
+			  {text: 'continuar', onPress: () => console.log('Ask me later pressed')},
+			  {text: 'Cancelar', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+			  
+			]
+			
+		)
 	}
 }
 
 const styles = StyleSheet.create({
   input1:{
-		width: 200,
-		height:30,
+		width: 300,
+		height:50,
 		marginTop:15,
 		padding: 15,
+		color: 'black',
+	},
+	enviar:{
+		backgroundColor:"#007ACC",
+		padding: 15,
 		
-
+		
 	}
 });
